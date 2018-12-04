56a4509a35f3efc6c3870111a2961fefad99fb3d
André Accioly <andreacciolyl@hotmail.com>
2018-12-04 16:20:50 -0200

    Inicio de estudo de CSS

--- polls/views.py
+++ polls/views.py
@@ -19,3 +19,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
+
+def camisas(request):
+	return render(request, 'polls/camisa.html')
